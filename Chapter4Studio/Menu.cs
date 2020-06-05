d3d10384725a47f0e5ed0d7e75644fc57f76c64d
Daryn DeMoss <darynbrennondemoss@gmail.com>
2020-06-05 17:13:45 -0400

    Add project files.

---
+++ Chapter4Studio/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter4Studio
+{
+    public class Menu
+    {      
+
+        public List<MenuItems> Items { get; set; }
+        public DateTime LastUpdated { get; set; }
+
+        public Menu(List<MenuItems> items, DateTime lastUpdated)
+        {
+            Items = items;
+            LastUpdated = lastUpdated;
+        }
+
+
+    }
+}
---
+++ Chapter4Studio/MenuItems.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter4Studio
+{
+    class MenuItems
+    {       
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool IsNew { get; set; }
+
+        public MenuItems(double price, string description, string category, bool isNew)
+        {
+            Price = price;
+            Description = description;
+            Category = category;
+            IsNew = isNew;
+        }
+    }
+}
